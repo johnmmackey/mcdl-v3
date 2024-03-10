509739b0015881eda8d589a1493930ffbf3307fd
John Mackey <john.m.mackey@gmail.com>
2024-03-10 15:32:16 -0400

    Removed unneeded CSS; added user profile

--- auth.ts
+++ auth.ts
@@ -19,16 +19,18 @@ export const {
     },
     async session({ session, user, token }) {
       console.log('session callback',session, user, token );
-      session.user.name = `${token.givenName} ${token.familyName}`;
-      //session.givenName = token.givenName;
+      session.profile = token.profile;
       return session
     },
     async jwt({ token, user, account, profile, isNewUser }) {
       console.log('jwt callback', token, user, account, profile, isNewUser);
       if(profile) {
         // user logged in and we have cognito profile
-        token.familyName = profile.family_name;
-        token.givenName = profile.given_name;
+        token.profile = {
+          familyName: profile.family_name,
+          givenName: profile.given_name,
+          groups: profile['cognito:groups']
+        }
       }
       return token
     }
