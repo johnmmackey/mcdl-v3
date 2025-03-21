fd9018add8dfb1e9bc3e7940395a4ba5754a3772
John Mackey <john.m.mackey@gmail.com>
2025-03-21 18:02:04 -0400

    changed logger

--- app/lib/logger.ts
+++ app/lib/logger.ts
@@ -1,18 +1,46 @@
-import pino from 'pino';
-import config from '@/loggerConfig.json'
+//import config from '@/loggerConfig.json'
 
-export const logger = pino(
-  {
-    level: process.env.PINO_LOG_LEVEL || config.main || 'info',
-    timestamp: pino.stdTimeFunctions.isoTime,
-  },
-);
+import winston from 'winston';
+import micromatch from 'micromatch';
 
-export const loggerFactory = ({ module, subModule }: { module: string, subModule?: string }) => {
-  let childLogger = logger.child({ module, subModule });
+let baseLogLevel = 'debug';
+let debugModulePatterns: string[] = [];
 
-  if ((<any>config)[module])
-    childLogger.level = (<any>config)[module];
-  return childLogger;
+const loggerFactory = ({ module, subModule }: { module: string, subModule?: string }) => {
+  const label = subModule ? `${module}:${subModule}` : `${module}`;
+
+  return winston.loggers.get(label, {
+    level: micromatch([label], debugModulePatterns).length ? 'debug' : baseLogLevel,
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.label({ label }),
+      winston.format.timestamp(),
+      winston.format.printf((info) => {
+        return `${info.timestamp} [${info.level}]: [${info.label}]: ${info.message}`;
+      })
+    )
+  });
 }
 
+const logger = loggerFactory({module: 'default'});
+
+const setBaseLevel = (level: string) => {
+  // have to set the level on any existing loggers and save this list in case new loggers are created
+  const loggerList = Array.from(winston.loggers.loggers.keys());
+  loggerList.forEach(m => winston.loggers.get(m).level = level);
+  baseLogLevel = level;
+}
+
+const setDebugModules = (patterns: string[]) => {
+  // have to set the level on any existing loggers and save this list in case new loggers are created
+  const loggerList = Array.from(winston.loggers.loggers.keys());
+  patterns.forEach(pat => {
+    micromatch(loggerList, pat).forEach(m => winston.loggers.get(m).level = 'debug');
+  });
+  debugModulePatterns = patterns;
+}
+
+export { logger, loggerFactory, setBaseLevel, setDebugModules };
